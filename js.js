02aa8d2a4401e6f83495fcda53e13615748a6425
Anch <d3tonny@gmail.com>
2022-09-10 00:06:24 +0300

    Циклы

--- js.js
+++ js.js
@@ -302,4 +302,37 @@
 //     console.log(i);
 // }
 //
-
+//  for (let i = 0; i < 3; i++) {
+//  console.log(i)
+//      for (let j = 0; j < 3; j++) {
+//          console.log(j)
+//          for (let k = 0; k < 3; k++) {
+//              console.log(k)
+//          }
+//      }
+// }
+//
+//
+// let result = '';
+// const length = 33;
+// for ( let i = 1; i < length; i++) {
+//     for (let j = 0; j < i; j++){
+//         result += '*'
+//     }
+//     result +='\n';
+// }
+// console.log(result);
+//
+// first: for (let i = 0; i < 3; i++) {
+//     console.log(`First level: ${i}`)
+//     for ( let j = 0; j < 3; j++) {
+//         console.log(`Second level: ${j}`)
+//         for ( let k = 0; k < 3; k++) {
+//             if (k === 2) {
+//                 break first;
+//             }
+//             console.log(`Second level: ${k}`)
+//         }
+//     }
+// }
+//
