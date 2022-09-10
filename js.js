0a31d748c20eb76e6dc2f9ba6ebfd9e0f6fb2828
Anch <d3tonny@gmail.com>
2022-09-10 11:07:02 +0300

    Практика №2

--- js.js
+++ js.js
@@ -336,3 +336,45 @@
 //     }
 // }
 //
+// Практика №2
+//
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+//
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+//
+// };
+//
+// if (personalMovieDB.count <= 10 && personalMovieDB.count >= 1) {
+//     console.log('Просмотрено довольно мало фильмов')
+// }
+// if (personalMovieDB.count > 10 && personalMovieDB.count <= 30) {
+//     console.log('Вы классический зритель')
+// }
+// if (personalMovieDB.count > 30) {
+//     console.log('Вы киноман')
+// }
+// if (personalMovieDB.count === 0) {
+//     console.log('Произошла ошибка')
+// }
+//
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = +prompt('На сколько оцените?', '');
+//
+//           if (a!== null && b != null && a != '' && b != '' && a.length < 50) {
+//               personalMovieDB.movies[a] = b;
+//               console.log('Finish')
+//           } else {
+//               console.log('Error');
+//               i--;
+//           }
+// }
+//
+// console.log(personalMovieDB);
+//
+//
