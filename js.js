68e494a43755fd11cca4e823dfb12966c3d03306
Anch <d3tonny@gmail.com>
2022-09-13 11:50:21 +0300

    ООП

--- js.js
+++ js.js
@@ -791,10 +791,35 @@
 // const newObject = {...q}
 // console.log(newObject)
 //
-
-
-
-
+// Основы ООП
+//
+// let str = 'some'
+// let strObj = new String(str)
+//
+// console.log(typeof(str))
+// console.log(typeof(strObj))
+//
+// console.dir([1, 2, 3])
+//
+// const soldier = {
+//     health: 400,
+//     armor: 100,
+//     sayHello: function () {
+//         console.log('Hello')
+//     }
+// }
+// const john = Object.create(soldier)
+//
+// const John = {
+//     health: 100
+// }
+//
+// John.__proto__ = soldier;
+// Object.setPrototypeOf(John, soldier)
+//
+// john.sayHello()
+//
+//
 
 
 
