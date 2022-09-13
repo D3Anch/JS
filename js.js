e517aa4225fe2083d077575cd8fd9424f6069510
Anch <d3tonny@gmail.com>
2022-09-13 23:41:09 +0300

    13.09

--- js.js
+++ js.js
@@ -902,7 +902,44 @@
 // personalMovieDB.toggleVisibleMyDB()
 //
 //
-
-
+// Динамическая типизация
+//
+// To String
+// console.log(typeof(String(null)))
+// console.log(typeof(null + ''))
+//
+// const num = 5
+// console.log('https://vk.com/catalog/' + num)
+//
+// const fontSize = 26 + 'px';
+//
+// to Number
+//
+// console.log(typeof(Number('4')))
+// console.log(typeof(+'5'))
+// console.log(typeof(parseInt('15px', 10)))
+//
+// let answ = +prompt('Введите число')
+//
+// to Boolean
+//
+// 0, null, '', undefined, NaN == false
+//
+// let switcher = null
+//
+// if (switcher) {
+//     console.log('Working...')
+// }
+//
+// switcher = 1
+//
+// if (switcher) {
+//     console.log('Working...')
+// }
+//
+//
+// console.log(typeof(Boolean('4')))
+// console.log(typeof(!!'4444444'))
+//
 
 
