d34cd3c02660ea1fc3293f47f8c663de816eb17f
Anch <d3tonny@gmail.com>
2022-09-14 09:47:49 +0300

    Лексическое окружение

--- js.js
+++ js.js
@@ -941,5 +941,34 @@
 // console.log(typeof(Boolean('4')))
 // console.log(typeof(!!'4444444'))
 //
-
-
+// Замыкание и Лексическое окружение
+//
+// let number = 5; debugger
+//
+// function logNumber() {
+//     let number = 4; debugger
+//     console.log(number)
+// }
+//
+// number = 6
+//
+// logNumber(); debugger
+//
+// function createCounter() {
+//     let counter = 0
+//
+//     const myFunction = function() {
+//         counter = counter + 1
+//         return counter
+//     }
+//
+//     return myFunction
+// }
+//
+// const increment = createCounter()
+// const c1 = increment()
+// const c2 = increment()
+// const c3 = increment()
+//
+// console.log(c1, c2, c3)
+//
